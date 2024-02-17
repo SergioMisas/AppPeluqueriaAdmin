1af3bcd0e528b2e7f0bf89eb1feea73f41cb22c1
sergiomisas <misas_virseda@hotmail.es>
2024-02-17 20:56:23 +0100

    Appointment recycler implemented in home

---
+++ app/src/main/java/dev/kuromiichi/apppeluqueriaadmin/models/Appointment.kt
@@ -0,0 +1,13 @@
+package dev.kuromiichi.apppeluqueriaadmin.models
+
+import java.util.Date
+
+data class Appointment(
+    val id: String = "",
+    val userUid: String = "",
+    val date: Date = Date(),
+    val time: Date = Date(),
+    val services: List<Service> = emptyList()
+) {
+    val duration: Int get() = services.sumOf { it.duration }
+}
