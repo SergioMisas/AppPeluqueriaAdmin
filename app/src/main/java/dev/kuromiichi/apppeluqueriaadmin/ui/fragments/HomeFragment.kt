13813bb08bbd53ab87be32181b7b01f8db7eedb9
kuromiichi <emmalikescoding@gmail.com>
2024-02-17 21:30:31 +0100

    date filter datepickers

--- app/src/main/java/dev/kuromiichi/apppeluqueriaadmin/ui/fragments/HomeFragment.kt
+++ app/src/main/java/dev/kuromiichi/apppeluqueriaadmin/ui/fragments/HomeFragment.kt
@@ -1,23 +1,28 @@
 package dev.kuromiichi.apppeluqueriaadmin.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
-import androidx.navigation.fragment.findNavController
 import dev.kuromiichi.apppeluqueriaadmin.R
 import dev.kuromiichi.apppeluqueriaadmin.adapters.RecyclerAppointmentAdapter
 import dev.kuromiichi.apppeluqueriaadmin.databinding.FragmentHomeBinding
 import dev.kuromiichi.apppeluqueriaadmin.listeners.AppointmentOnClickListener
 import dev.kuromiichi.apppeluqueriaadmin.models.Appointment
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Calendar.MONDAY
 import java.util.Date
+import java.util.Locale
 
 class HomeFragment : Fragment(), AppointmentOnClickListener {
     private var _binding: FragmentHomeBinding? = null
@@ -29,7 +34,6 @@ class HomeFragment : Fragment(), AppointmentOnClickListener {
     private lateinit var adapter: RecyclerAppointmentAdapter
     private var beginDate: Date? = null
     private var endDate: Date? = null
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -109,13 +113,73 @@ class HomeFragment : Fragment(), AppointmentOnClickListener {
 
     override fun onAppointmentClick(appointment: Appointment) {
         TODO("Not yet implemented")
-
-
     }
 
     private fun setButtons() {
         binding.fabSettings.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_settingsFragment)
         }
+
+        binding.btnDateFrom.setOnClickListener {
+            datePickerDialog(binding.etDateFrom)
+        }
+
+        binding.btnDateTo.setOnClickListener {
+            datePickerDialog(binding.etDateTo)
+        }
+
+        binding.btnFilterOff.setOnClickListener {
+            resetFilters()
+        }
+    }
+
+    private fun datePickerDialog(editText: EditText) {
+        val dialog = MaterialDatePicker.Builder.datePicker()
+            .setTitleText(getString(R.string.choose_date))
+            .setCalendarConstraints(
+                CalendarConstraints.Builder()
+                    .setFirstDayOfWeek(MONDAY)
+                    .build()
+            ).build()
+        dialog.show(childFragmentManager, "DATE_PICKER")
+
+        dialog.addOnPositiveButtonClickListener {
+            if (dialog.selection != null) {
+                editText.setText(
+                    SimpleDateFormat(
+                        "dd/MM/yyyy",
+                        Locale.getDefault()
+                    ).format(dialog.selection)
+                )
+                updateDates()
+            }
+        }
+    }
+
+    private fun updateDates() {
+        if (binding.etDateFrom.text.toString().isNotEmpty()) beginDate = SimpleDateFormat(
+            "dd/MM/yyyy",
+            Locale.getDefault()
+        ).parse(binding.etDateFrom.text.toString())
+        if (binding.etDateTo.text.toString().isNotEmpty()) endDate = SimpleDateFormat(
+            "dd/MM/yyyy",
+            Locale.getDefault()
+        ).parse(binding.etDateTo.text.toString())
+
+        if (beginDate != null && endDate != null && beginDate!! > endDate!!) {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.invalid_date),
+                Toast.LENGTH_SHORT
+            ).show()
+            resetFilters()
+        }
+    }
+
+    private fun resetFilters() {
+        beginDate = null
+        endDate = null
+        binding.etDateFrom.setText("")
+        binding.etDateTo.setText("")
     }
 }
